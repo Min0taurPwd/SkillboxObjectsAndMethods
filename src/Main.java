fbaa85b717e3f03f0e29755640fee6dacab99ac4
Min0taurPwd <minotaurworld@mail.ru>
2023-11-14 01:51:27 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Basket basket = new Basket();
+        basket.add("Milk", 40, 50.5);
+        basket.add("Cola", 40, 0.4);
+        basket.print("Milk");
+    }
+}
